e20cd709d8246bdb93889711d635f26228d905af
Kartik Anand Pant <kpant14@gmail.com>
2024-05-14 11:08:37 -0400

    Add foxglove config

--- launch/fdir_sitl_launch.py
+++ launch/fdir_sitl_launch.py
@@ -1,10 +1,11 @@
 import os
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 
 
 
@@ -36,7 +37,16 @@ def generate_launch_description():
 			{'ros_ns' : ros_ns_str}
 		])
 	
+	foxglove_studio = ExecuteProcess(cmd=["foxglove-studio"])
+
+	foxglove_bridge = IncludeLaunchDescription(XMLLaunchDescriptionSource(
+        os.path.join(
+        get_package_share_directory("mas_fdir"),
+        "launch/foxglove_bridge.launch",))
+    )
 	return LaunchDescription([
 		fdir_node,
-		interagent_pub
+		interagent_pub,
+		foxglove_studio,
+		foxglove_bridge
 	])
--- setup.py
+++ setup.py
@@ -3,17 +3,33 @@ import os
 from glob import glob
 
 package_name = 'mas_fdir'
+data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ]
+
+def package_files(data_files, directory_list):
+    paths_dict = {}
+    for directory in directory_list:
+        for (path, directories, filenames) in os.walk(directory):
+            for filename in filenames:
+                file_path = os.path.join(path, filename)
+                install_path = os.path.join('share', package_name, path)
+                if install_path in paths_dict.keys():
+                    paths_dict[install_path].append(file_path)
+                else:
+                    paths_dict[install_path] = [file_path]
+    for key in paths_dict.keys():
+        data_files.append((key, paths_dict[key]))
+
+    return data_files
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-    ],
+    data_files=package_files(data_files, ['launch/']),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Vishnu',
